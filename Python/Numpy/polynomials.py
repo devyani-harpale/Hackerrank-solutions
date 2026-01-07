5422685879673d26b77bc77846979dd911feddae
devyani-harpale <harpaledevyani5054@gmail.com>
2026-01-07 17:52:04 +0530

    Polynomials

---
+++ Python/Numpy/polynomials.py
@@ -0,0 +1,19 @@
+'''Task
+You are given the coefficients of a polynomial P.
+Your task is to find the value of Pat point 2.
+Input Format
+The first line contains the space separated value of the coefficients in P.
+The second line contains the value of x.
+Output Format
+Print the desired value.
+Sample Input
+1.1 2 3
+0
+Sample Output
+3.0'''
+import numpy
+
+P = list(map(float, input().split()))
+x = int(input())
+
+print(numpy.polyval(P,x))
