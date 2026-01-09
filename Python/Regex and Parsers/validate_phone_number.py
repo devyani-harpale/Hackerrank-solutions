bc5c3807d34363266c633ecf1b641757e0ea6f0c
devyani-harpale <harpaledevyani5054@gmail.com>
2026-01-09 13:56:32 +0530

    Valid Phone Number missing testcase

--- Python/Regex and Parsers/validate_phone_number.py
+++ Python/Regex and Parsers/validate_phone_number.py
@@ -22,7 +22,7 @@ NO'''
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 import re
 N = int(input())
-regex_pattern = r"^[789]\d{9}"
+regex_pattern = r"^[789]\d{9}$"
 
 for _ in range(N):
     nos = input()
